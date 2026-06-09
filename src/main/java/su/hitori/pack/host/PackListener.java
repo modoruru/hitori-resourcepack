584f2f2084f3eb5dbb96c1466297f6a824f440f4
justlofe <lofikpc@gmail.com>
2026-06-09 14:58:28 +0700

    Revert changes that was not intended to commit

--- src/main/java/su/hitori/pack/host/PackListener.java
+++ src/main/java/su/hitori/pack/host/PackListener.java
@@ -5,7 +5,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import su.hitori.api.util.Text;
 import su.hitori.pack.PackConfiguration;
@@ -18,7 +20,6 @@ public final class PackListener implements Listener {
         this.packServer = packServer;
     }
 
-    /*
     @EventHandler
     private void onPlayerDamage(EntityDamageEvent event) {
         if(event.getEntity() instanceof Player player && player.getResourcePackStatus() != PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) {
@@ -32,7 +33,6 @@ public final class PackListener implements Listener {
             event.setCancelled(true);
         }
     }
-    */
 
     @EventHandler
     private void onResourcePack(PlayerResourcePackStatusEvent event) {
@@ -41,7 +41,7 @@ public final class PackListener implements Listener {
         var cfg = PackConfiguration.I;
         switch (event.getStatus()) {
             case DECLINED, DISCARDED -> player.kick(Text.create(cfg.allowResourcepackToPlay));
-            // case FAILED_DOWNLOAD, INVALID_URL, FAILED_RELOAD  -> player.kick(Text.create(cfg.errorInstallingResourcepack));
+            case FAILED_DOWNLOAD, INVALID_URL, FAILED_RELOAD  -> player.kick(Text.create(cfg.errorInstallingResourcepack));
             default -> {}
         }
     }
